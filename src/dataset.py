6b5e5c92f1ba5d2e1b4ea2ecdc48b7f8aff569fc
chenhongyu <chenhongyu@bytedance.com>
2018-06-05 21:21:18 +0800

    feat: change dataset class to apply training data.
    Change-Id: I8367a7a807c6be4b1e320289d3325c63945dae04

--- src/dataset.py
+++ src/dataset.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import random
 
+
 class DataSet(object):
     def __init__(self, data_dir, batch_size, label_dim, max_size=-1):
         self.DATA_SIZE = 224
@@ -12,57 +13,64 @@ class DataSet(object):
         self.gen_image_list()
         self.shuffle_data()
         self.cur_index = 0
+        self.end_index = 0
         self.label_dim = label_dim
 
     def gen_image_list(self):
         self.image_list = []
         self.total_images = 0
-        for file_name in os.listdir(self.data_dir):
-            if (file_name.endswith("jpg")):
-                file_size = os.path.getsize(self.data_dir + "/" + file_name)
-                if file_size / 1024 < 5:
-                    continue
-                self.image_list.append(file_name)
-                self.total_images += 1
-                if (self.max_size > 0 and self.total_images >= self.max_size):
-                    break
-        self.total_images = (self.total_images // self.batch_size) * self.batch_size
+        for dir_name in os.listdir(self.data_dir):
+            fi_d = os.path.join(self.data_dir, dir_name)
+            for file_name in os.listdir(fi_d):
+                if file_name.endswith("jpg"):
+                    # file_size = os.path.getsize(self.data_dir + "/" + file_name)
+                    # if file_size / 1024 < 5:
+                    #     continue
+                    self.image_list.append(dir_name + '/' + file_name)
+                    self.total_images += 1
+                    if (self.max_size > 0 and self.total_images >= self.max_size):
+                        break
+        self.total_images = (self.total_images //
+                             self.batch_size) * self.batch_size
 
-        self.total_batches = (self.max_size // self.batch_size) if self.max_size > 0 else (self.total_images // self.batch_size)
-        #self.total_batches -= 10
+        self.total_batches = (self.max_size // self.batch_size) \
+            if self.max_size > 0 else (self.total_images // self.batch_size)
+        # self.total_batches -= 10
 
     def read_image(self, image_name):
         image_path = os.path.join(self.data_dir, image_name)
         img_data = cv2.imread(image_path)
-#        img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
-
-#        cv2.imwrite(image_path, img_data)
-#        img_data = cv2.imread(image_path)
-        height, weight, channels = img_data.shape
+        # img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
+        # cv2.imwrite(image_path, img_data)
+        # img_data = cv2.imread(image_path)
+        # height, weight, channels = img_data.shape
 
         #original_img = cv2.resize(img_data, (self.DATA_SIZE, self.DATA_SIZE))
-        original_img = cv2.resize(img_data, (self.DATA_SIZE, self.DATA_SIZE)) / 255.0
+        original_img = cv2.resize(
+            img_data, (self.DATA_SIZE, self.DATA_SIZE)) / 255.0
         original_img = original_img * 2 - 1
-        return original_img, image_path.split("_")[0][-1]
+        return original_img, image_path.split("_")[0][-2:]
 
     def shuffle_data(self):
         # test set should not be shuffled
         random.shuffle(self.image_list)
 
     def next_batch(self):
-        self.end_index = min([self.cur_index + self.batch_size, self.total_images])
+        self.end_index = min(
+            [self.cur_index + self.batch_size, self.total_images])
         array_original_img = []
         array_label = []
         for i in range(self.cur_index, self.end_index):
-#            print(self.image_list[i])
+            # print(self.image_list[i])
             original_img, label_img = self.read_image(self.image_list[i])
+            # print("label_img: ", label_img)
             array_original_img.append(original_img)
             label = np.zeros([self.label_dim])
             label[int(label_img) - 1] = 1
             array_label.append(label)
 
         self.cur_index += self.batch_size
-        if (self.cur_index >= self.total_images):
+        if self.cur_index >= self.total_images:
             self.cur_index = 0
             self.shuffle_data()
 
@@ -70,10 +78,9 @@ class DataSet(object):
 
 
 if __name__ == "__main__":
-    dataset = DataSet("./img", 2, False)
-
+    DATA_SET = DataSet("./training", 2, 50, 500)
     for i in range(10):
-        original_img, label = dataset.next_batch()
-        #cv2.imwrite("hahaha.jpg", original_img[0])
-        print "label:", label[0]
-        print(original_img.shape, label.shape)
+        original_img, label = DATA_SET.next_batch()
+        # cv2.imwrite("hahaha.jpg", original_img[0])
+        print("label1:", label[0])
+        # print(original_img.shape, label.shape)
